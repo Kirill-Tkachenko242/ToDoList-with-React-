47680c69335eafeb870acc2295bfcf7309312a56
Kirill Tkachenko <kirilltkacenko37@gmail.com>
2024-07-18 11:52:35 +0300

    provider1

---
+++ src/context/TaskProvider.js
@@ -0,0 +1,50 @@
+import React, {useMemo, useState} from 'react';
+import TaskContext from './TaskContext';
+
+const TaskProvider = () => {
+    const [tasks, setTasks] = useState([]);
+    const [filter, setFilter] = useState('all');
+
+    const addTask = (taskName) => {
+        setTasks([...tasks, { name: taskName, completed: false }]);
+    };
+
+    const deleteTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
+
+    const toggleTaskCompletion = (index) => {
+        const newTasks = tasks.map((task, i) => {
+            if (i === index) {
+                return { ...task, completed: !task.completed };
+            }
+            return task;
+        });
+        setTasks(newTasks);
+    };
+
+    const filteredTasks = useMemo(() => {
+        return tasks.filter((task) => {
+            if (filter === 'active') return !task.completed;
+            if (filter === 'complited') return task.completed;
+        });
+    }, [tasks, filter]);
+
+    const contextValue = useMemo(() => ({
+        tasks: filteredTasks,
+        addTask,
+        deleteTask,
+        toggleTaskCompletion,
+        filter,
+        setFilter,
+    }), [filteredTasks, filter]);
+
+    return (
+        <TaskContext.Provider value={contextValue}>
+            {children}
+        </TaskContext.Provider>
+    );
+};
+
+
+export default TaskProvider;
